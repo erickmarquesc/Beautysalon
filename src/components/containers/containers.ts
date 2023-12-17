619118fd0aabe6cc737a94ae1d5a8ef0a7005e30
Erick Marques <erick.marquesc@hotmail.com>
2023-12-17 18:15:56 -0300

    feat: :sparkles: create a new component

---
+++ src/components/containers/containers.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: auto;
+  padding: 20px 0;
+`;
+
+export const Content = styled.div`
+  width: 100%;
+  display: flex;
+  margin: 0 auto;
+  padding: 0 15px;
+  max-width: 1120px;
+  justify-content: space-between;
+`;
