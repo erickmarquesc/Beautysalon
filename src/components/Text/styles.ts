bdeeeded2719ce0fd0472aea75ebca4109aa0aa0
Erick Marques <erick.marquesc@hotmail.com>
2023-12-17 21:39:20 -0300

    feat: :sparkles: create a new component

---
+++ src/components/Text/styles.ts
@@ -0,0 +1,54 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+  font: 700 3.6rem 'Poppins', sans-serif;
+  color: ${(props) => props.theme['green-dark']};
+  text-transform: capitalize;
+
+  text-transform: none;
+  overflow: hidden;
+
+  &:after {
+    content: '';
+    display: inline-block;
+    width: 0;
+  }
+
+  &:first-letter {
+    text-transform: uppercase;
+  }
+`
+export const SubTitle = styled.h1`
+  font: 400 1.8rem 'DM Sans', sans-serif;
+  color: ${(props) => props.theme['gray']};
+
+  text-transform: none;
+  overflow: hidden;
+
+  &:after {
+    content: '';
+    display: inline-block;
+    width: 0;
+  }
+
+  &:first-letter {
+    text-transform: uppercase;
+  }
+`
+export const Description = styled.h1`
+  font: 400 1.6rem 'DM Sans', sans-serif;
+  color: ${(props) => props.theme['gray']};
+
+  text-transform: none;
+  overflow: hidden;
+
+  &:after {
+    content: '';
+    display: inline-block;
+    width: 0;
+  }
+
+  &:first-letter {
+    text-transform: uppercase;
+  }
+`
