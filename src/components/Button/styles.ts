86dc1964101d2570e83a0beb9e7c877b68fee9e9
Erick Marques <erick.marquesc@hotmail.com>
2023-12-17 21:38:25 -0300

    feat: :sparkles: create a new component

---
+++ src/components/Button/styles.ts
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+
+/**
+ * @component @example
+ * // Exemplo de uso:
+ * <Button>
+      <img src={icon} />
+        <label>
+          text button
+        </label>
+    </Button>
+ */
+export const Button = styled.button`
+  gap: 12px;
+  border: none;
+  display: flex;
+  overflow: hidden;
+  flex-wrap: nowrap;
+  border-radius: 4px;
+  flex-direction: row;
+  align-items: center;
+  text-transform: none;
+  padding: 14px 32px 15px 32px;
+  font: 700 1.6rem 'DM Sans', sans-serif;
+  color: ${ (props) => props.theme['white'] };
+  background-color: ${ (props) => props.theme['green'] };
+
+  cursor: pointer;
+
+  transition: 1s;
+
+  &:hover{
+    filter: brightness(80%);
+  }
+  
+  label{
+    cursor: pointer;
+
+    &:after {
+      content: '';
+      display: inline-block;
+      width: 0;
+    }
+    
+    &:first-letter {
+      text-transform: uppercase;
+    }
+  }
+`
