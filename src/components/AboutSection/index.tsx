86fcceb55350f59e4ee90e6245e27271fec003cf
Erick Marques <erick.marquesc@hotmail.com>
2023-12-18 11:53:44 -0300

    feat: :sparkles: create a new component

---
+++ src/components/AboutSection/index.tsx
@@ -0,0 +1,33 @@
+import imgAbout from "../../assets/img/ImgAbout.svg";
+import { Description, Title } from "../Text/styles";
+import { ContentAboutSection } from "./styles";
+
+export const AboutSection = () => {
+  return (
+    <ContentAboutSection>
+      <img src={imgAbout} alt="" />
+      <div>
+        <Title>
+          sobre nós
+        </Title>
+        <div className="ContentDescription">
+          <Description>
+            beautysalon, seu espaço exclusivo para cuidados capilares em São Paulo!
+            Há mais de 10 anos no mercado, somos um salão especializado em tratamentos naturais
+            fundado por três mulheres brasileiras apaixonadas por realçar a beleza autêntica dos cabelos.
+          </Description>
+          <Description>
+            em pleno coração de São Paulo, destacamo-nos por oferecer terapia capilar, cortes e tratamentos personalizados.
+            Aqui, acreditamos na saúde natural dos seus cabelos e proporcionamos uma experiência única,
+            guiada pela expertise das nossas fundadoras.
+          </Description>
+          <Description>
+            no Beautysalon, a feminilidade, força e graça das mulheres se encontram.
+            Confie em nós para cuidar da saúde dos seus cabelos em um ambiente acolhedor e elegante.
+            Venha viver a experiência Beautysalon, onde a excelência se une à tradição e inovação.
+          </Description>
+        </div>
+      </div>
+    </ContentAboutSection>
+  )
+}
---
+++ src/components/AboutSection/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import { Content } from "../containers/containers";
+
+export const ContentAboutSection = styled(Content)`  
+  max-width: 1350px;
+  align-items: center;
+  
+  .ContentDescription{
+    gap: 12px;
+    display: flex;
+    max-width: 610px;
+    flex-direction: column;
+  }
+`
