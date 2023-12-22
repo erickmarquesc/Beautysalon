ccc8e8d1fd21a6dddbcefae94c4591c9a3b0abe5
Erick Marques <erick.marquesc@hotmail.com>
2023-12-22 15:05:19 -0300

    feat: :sparkles: create a new component

---
+++ src/components/ContactSection/styles.ts
@@ -0,0 +1,52 @@
+import styled from "styled-components";
+import { Content } from "../containers/containers";
+
+export const ContentContactSection = styled(Content)`
+
+  align-items: center;
+
+  .textContactSection{
+    max-width: 404px;
+
+    display: flex;
+    flex-direction: column;
+    gap: 24px;
+
+   h2{
+      max-width: 364px;
+    }
+
+    .ContenttextContactSection{
+      display: flex;
+      flex-direction: column;
+      gap: 12px;
+    }
+  }
+
+  .iconsContactSection{
+    display: flex;
+    flex-direction: column;
+    gap: 32px;
+    
+    li{
+      all: inherit;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      gap: 12px;
+
+    }
+    
+    .typeEmail{
+      &:after {
+        content: '';
+        display: inline-block;
+        width: 0;
+      }
+
+      &:first-letter {
+        text-transform: lowercase;
+      }
+    }
+  }
+`
