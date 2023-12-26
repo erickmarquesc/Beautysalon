75547c3de5b29ec3e249fbfc55db3090d2d91e6c
Erick Marques <erick.marquesc@hotmail.com>
2023-12-26 15:55:31 -0300

    style: :lipstick: update ui

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -9,6 +9,7 @@ export const GlobalStyle = createGlobalStyle`
 
   html{
     font-size: 10px;
+    scroll-behavior: smooth;
   }
   
   body {
@@ -16,4 +17,17 @@ export const GlobalStyle = createGlobalStyle`
     font-family: 'Poppins', sans-serif;
     background: ${(props) => props.theme['backgound']};
   };
+
+    /* Estilizando a barra de rolagem */
+  ::-webkit-scrollbar {
+      width: 8px; /* Largura da barra de rolagem no Chrome/Safari/Edge */
+  }
+
+  ::-webkit-scrollbar-thumb {
+      background-color: ${(props) => props.theme['green']}; /* Cor do "ponteiro" (thumb) da barra de rolagem no Chrome/Safari/Edge */
+  }
+
+  ::-webkit-scrollbar-track {
+      background-color: ${(props) => props.theme['gray-light']}; /* Cor de fundo da trilha da barra de rolagem no Chrome/Safari/Edge */
+  }
 `
